fbebd8e90bd13578408f27d1db54a3845ac36372
Naomi Dennis <njdennis94@gmail.com>
2018-04-18 16:06:58 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :author
   belongs_to :category
+
+  def category_name=(name)
+  self.category = Category.find_or_create_by(name: name)
+end
+
+def category_name
+   self.category.name
+end
 end
