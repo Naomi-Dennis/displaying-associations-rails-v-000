53847646e12395a40ca5a575bcfab002e4e0103f
Naomi Dennis <njdennis94@gmail.com>
2018-04-18 16:12:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:category, :description, :title)
+    params.require(:post).permit(:category_name, :description, :title)
   end
 end
